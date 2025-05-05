366ed5106ff66e758c9745c56b4affbef0c45308
adji4 <sambadjianta@gmail.com>
2025-05-05 18:54:10 +0000

    -

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
-var buttons = document.getElementsByClassName("fas")
-var quantities = document.getElementsByClassName("quantity")
+var buttons = document.getElementsByClassName("fas"); // on recupere tous les éléments qui ont une classe "fas" 
+var buttonsArray = Array.from(buttons); // on convertis notre collection d'éléments en un tableau 
+var itemTotalPrice = document.getElementById("total") // on recupere l'élément total price
 
-Array.from(buttons).forEach(button => {
+buttonsArray.forEach(button => {
     button.addEventListener("click", ()=>{
+
+        var itemQuantity;
+        var quantity; 
+
         if(button.classList.contains("fa-plus-circle")){
 
-        var quantity = button.nextElementSibling
-        
-        let value = quantity.innerHTML
-        quantity.innerHTML = parseInt(value) + 1
+            itemQuantity = button.nextElementSibling
+            quantity = itemQuantity.innerHTML
+            itemQuantity.innerHTML = parseInt(quantity) + 1
+            var unitPrice = button.parentNode.previousElementSibling.innerHTML
+            unitPrice = unitPrice.replace("$", "") // on supprime le dollars $
+            unitPrice = parseInt(unitPrice)
+            itemTotalPrice.innerHTML = parseInt(itemTotalPrice.innerHTML) + unitPrice
+
+            console.log(unitPrice)
         }
         
+        else if(button.classList.contains("fa-minus-circle")){
+            itemQuantity = button.previousElementSibling;
+            quantity = itemQuantity.innerHTML;
+            quantity = parseInt(quantity);
+            if (quantity > 0){
+                itemQuantity.innerHTML = quantity -1
+                var unitPrice = button.parentNode.previousElementSibling.innerHTML
+                unitPrice = unitPrice.replace("$", "") // on supprime le dollars $
+                unitPrice = parseInt(unitPrice)
+                itemTotalPrice.innerHTML = parseInt(itemTotalPrice.innerHTML) - unitPrice
+    
+            }
+
+        
+        }
+        else if (button.classList.contains("fa-trash-alt")){
+            var pu = button.parentElement.previousElementSibling.previousElementSibling.innerHTML
+            var itemQty = button.parentElement.previousElementSibling.firstElementChild.nextElementSibling
+            var qty = itemQty.innerHTML
+            pu = pu.replace("$", "")
+            pu = parseInt(pu)
+            qty = parseInt(qty)
+            var ptArticle = qty * pu 
+            itemTotalPrice.innerHTML = parseInt(itemTotalPrice.innerHTML) - ptArticle // conversion du chaine de caractere en entier avec parseint avant soustraction 
+            itemQty.innerHTML = 0
+        }
+        else if(button.classList.contains("fa-heart")){
+            button.style.color = "red"
+
+        }
     })   
 });
+
+function updateTotalPrice(button) {
+    var unitPrice = button.parentNode.previousElementSibling.innerHTML
+    unitPrice = unitPrice.replace("$", "") // on supprime le dollars $
+    unitPrice = parseInt(unitPrice)
+    itemTotalPrice.innerHTML = parseInt(itemTotalPrice.innerHTML) + unitPrice
+
+}
